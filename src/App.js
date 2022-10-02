337ef454710d1a710b41d1e3e7a557bf2a317a9f
biist <72079769+JanneOhtonen@users.noreply.github.com>
2022-10-02 20:10:08 +0300

    kk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+
+
 
 function App() {
+
+  const [age, setAge] = useState()
+  const [hiValue, setHi] = useState(0)
+  const [loValue, setLo] = useState(0)
+
+  function calculate(e) {
+    e.preventDefault()
+    const hi = (220 - age) * 0.85
+    const lo = (220 - age) * 0.65
+    setHi(hi)
+    setLo(lo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <h1>Heart rate limits calculator</h1>
+      <form onSubmit={calculate}>
+        <div>
+          <label>Age</label>
+        </div>
+        <div>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label>Heart rate limit</label>
+        </div>
+        <div>
+          <output type="number">{loValue}-{hiValue}</output>
+        </div>
+        <button>Calculate</button>       
+      </form>
     </div>
   );
 }
